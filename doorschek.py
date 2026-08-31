26c95c5db8cc20a9cbfedec516bad99776162c86
Santiago Escobar <zanescobar0@gmail.com>
2026-08-31 08:25:02 -0300

    update doorschek.py

--- doorschek.py
+++ doorschek.py
@@ -30,15 +30,26 @@ def busqueda_door(door_number):
 # este sistema de busqueda binaria es eficiente hasta que te topas con busquedas de de numeros enormes donde tengamos que hacerla mas eficiente 
 def Efficient_Binary_Search():
     search_number = int(input("Enter the door number to search: ")) #entrada del número de puerta a buscar
-    lst = check_doors() #llamado a la función check_doors
+    search_lst = check_doors() #llamado a la función check_doors
     lst_start_number = 1 #inicio de la lista
-    lst_end_number = len(lst) - 1 #fin de la lista
-    def search_complexes():
+    lst_end_number = len(search_lst) - 1 #fin de la lista
+    def create_even_list():
         search_number_between = list(range(lst_start_number, lst_end_number + 1)) #creación de lista de búsqueda
         search_number_betweem = [num for num in search_number_between if num % 2 == 0] #filtrado de números pares
         return search_number_betweem #retorna la lista de búsqueda
+    filtered_range = create_even_list() #llamado a la función create_even_list
     while True: #bucle infinito para la búsqueda
-        number = search_number #asignación del número de búsqueda
-        if search_complexes < number > search_complexes: #condición para verificar si el número está en la lista de búsqueda
-            print(f"Door {number} is open.") #retorna que la puerta está abierta
+        if  search_number in search_lst: #si el número de búsqueda es mayor que el número de búsqueda complejo
+            closet = min(filtered_range, key=lambda x: abs(x - search_number)) #busca el número más cercano
+            diference = abs(closet - search_number) #calcula la diferencia
+            return f"Door {search_number-diference} Open  "
+        if search_number in search_lst: #si el número de búsqueda es menor que el número de búsqueda complejo
+            closet = min(filtered_range, key=lambda x: abs(x - search_number)) #busca el número más cercano
+            diference = abs(closet - search_number) #calcula la diferencia
+            return f"Door {search_number+diference} Open  "
+        if search_number in search_lst: #si el número de búsqueda es igual al número de búsqueda complejo
+            return f"Door {search_number} is open." #retorna que la puerta está abierta
             break #rompe el bucle
+        else :
+            return f"Door {search_number} not found." #retorna que la puerta no se encontró
+print(Efficient_Binary_Search()) #llamado a la función Efficient_Binary_Search
